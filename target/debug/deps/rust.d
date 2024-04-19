b5339b2ac787c0e0fb4debf031f0b2c4b8ddde7b
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-04-19 15:23:26 +0200

    Creating the repo for the Rust_WSAM project

--- target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Bassel Taha\Desktop\Angular-Clips\rust\target\debug\deps\rust.exe: src\main.rs
-
-C:\Users\Bassel Taha\Desktop\Angular-Clips\rust\target\debug\deps\rust.d: src\main.rs
-
-src\main.rs:
--- target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Bassel\ Taha\Desktop\Angular-Clips\rust\target\debug\rust.exe: C:\Users\Bassel\ Taha\Desktop\Angular-Clips\rust\src\main.rs
