06c2391aafee22f9e1c1dde97af5878f4eceb6f1
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-04-20 19:13:54 +0200

    Some error in installing web-pack using the npm tool

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+//the webpack.config.js file is used to configure the webpack bundler to compile the rust code and the js code together
+
+//the classes and plugins used in the webpack.config.js file are
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const wasmToolPlugIn = require('@wasm-tool/wasm-pack-plugin');
 const { experiments } = require('webpack');
+
+
 module.exports = {
   entry: './public/main.js',
   output: {
@@ -15,10 +20,9 @@ module.exports = {
         }
     ),
 
-    // wasmTool is used so that webpack can compile rust code
-    //todo must add the wasmToolPlugIn in the cargo.toml in the [lib] section so that rust know to create a creat with suport for extrnal languages
+    // wasmTool is used so that webpack can compile rust code todo must add the wasmToolPlugIn in the cargo.toml in the [lib] section so that rust know to create a creat with suport for extrnal languages
     new wasmToolPlugIn({
-      crateDirectory : path(__dirname , ".")
+      crateDirectory: path.resolve(__dirname , ".")
     }) 
 ],
 //todo must add the experiment features in the webpack.config.js file to enable rust compilation feature as its an experimental feature in webpack
