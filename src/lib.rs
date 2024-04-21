064eb982b9c9f5c02d23eb89915d7110fb7639fe
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-04-21 23:16:15 +0200

    using the base64 cargo package and logging the proccess for to know whenthe image is decoded

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 ;
+use base64::decode;
 
 //exporting the Gray_Scale function to the JS side of the code using the wasm_bindgen macro attribute
 #[wasm_bindgen]
@@ -7,5 +8,8 @@ pub fn Gray_Scale(encoded_Image : &str)
 {
     //using the log function from the web_sys cargo package to export a console log function to the JS side of the code and print the resault 
     //adding the into function to convert the string to a JsValue as the log_1 finction takes a JsValue as an argument
-    log_1(&encoded_Image.into());
+    log_1(&"the GrayScale function is called".into());
+    //decoding the base64 encoded image to a vector of bytes
+    let decoded_Image = decode(encoded_Image).unwrap();
+    log_1(&"the image is decoded and logged to the console".into())
 }
