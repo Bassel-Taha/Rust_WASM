8a84f91013c345a8438fef1e56169600d16c348b
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-04-22 22:33:38 +0200

    correcting som submission errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 ;
 use base64::decode;
-use image::load_from_memory;
-use image::load_from_memory;
+use image::{load_from_memory, DynamicImage};
 
 //exporting the Gray_Scale function to the JS side of the code using the wasm_bindgen macro attribute
 #[wasm_bindgen]
@@ -13,8 +12,11 @@ pub fn Gray_Scale(encoded_Image : &str)
     log_1(&"the GrayScale function is called".into());
     //decoding the base64 encoded image to a vector of bytes
     let decoded_Image = decode(&encoded_Image).unwrap();
+
     log_1(&"the image is decoded and logged to the console".into());
     //loading the image from the memory
     let img = load_from_memory(&decoded_Image).unwrap();
     log_1(&"the image is loaded from the memory".into());
+
+    log_1(&"the image is decoded and logged to the console".into());   
 }
