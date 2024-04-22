9ddda78c8be07606fbc47b3445bb3bd17eeb2ea5
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-04-22 23:39:53 +0200

    -adding the mutable buffer object witch will store the data during compilation and converting the image from binary to image -encoding the image to base64 again to retrun it to JS file - adding the metadata before returning it to the JS File so that the browser can read and undertand the data and display it -setting the attribute of the image tag in the dom to the returned value from the Lib.rs without changing it as the browser understands the base 64 encoding with the metadata writen in the string object

--- public/main.js
+++ public/main.js
@@ -1,40 +1,43 @@
 async function init() {
-const input = document.getElementById('upload')
+    const input = document.getElementById('upload')
 
-//creating a variable to store the rustapp 
-let rustapp = null
+    //creating a variable to store the rustapp 
+    let rustapp = null
 
-//using a try catch block to try getting the rustapp and if it fails, it will catch the error and log it to the console
-try {
-    rustapp = await import('../pkg')
-    console.log(rustapp)
-}
-catch(e)
-{
-    //logging the error and returning to stop the execution of the function
-console.log(e);
-return;
-}
+    //using a try catch block to try getting the rustapp and if it fails, it will catch the error and log it to the console
+    try {
+        rustapp = await import('../pkg')
+        console.log(rustapp)
+    }
+    catch (e) {
+        //logging the error and returning to stop the execution of the function
+        console.log(e);
+        return;
+    }
 
-// Create a new FileReader object that can read the file as string to pass the string to the rust files
-let fileReader = new FileReader(); 
+    // Create a new FileReader object that can read the file as string to pass the string to the rust files
+    let fileReader = new FileReader();
 
-// When the input changes, read the file
-input.addEventListener('change', ()=>{
-    //using the readAsDataURL method to read the file as a string with the base64 encoding to be easy for transfering the string to the rust files
-    fileReader.readAsDataURL(input.files[0])
-})
+    // When the input changes, read the file
+    input.addEventListener('change', () => {
+        //using the readAsDataURL method to read the file as a string with the base64 encoding to be easy for transfering the string to the rust files
+        fileReader.readAsDataURL(input.files[0])
+    })
 
-// When the file is loaded, remove the metadata of the file
-fileReader.onloadend = ()=>{
-    //the resault is encoded using base64 encoding 
-    let base64File = fileReader.result
-    //removing the metadata of the file
-    let fileWithoutMetadata = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, "");
+    // When the file is loaded, remove the metadata of the file
+    fileReader.onloadend = () => {
+        //the resault is encoded using base64 encoding 
+        let base64File = fileReader.result
+        //removing the metadata of the file
+        let fileWithoutMetadata = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, "");
 
-    //calling the rustapp module to call th ecompiled functions in the rust files
-    rustapp.Gray_Scale(fileWithoutMetadata);    
-}   
+        //calling the rustapp module to call th ecompiled functions in the rust files
+        let finalImageDataURL = rustapp.Gray_Scale(fileWithoutMetadata);
+        //displaying the image retruned from rust in the image tag in the dom using the base64 encoding as the browser can display the image using the base64 encoding
+        let imageTag = document.getElementById("new-img").setAttribute(
+            "src", finalImageDataURL
+        )
+    }
 }
 
 init()
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+use image::codecs::png;
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 ;
-use base64::decode;
+use base64::{decode , encode};
 use image::{load_from_memory, DynamicImage};
+use image::ImageFormat::Png;
+use std::io::Cursor;
 
 //exporting the Gray_Scale function to the JS side of the code using the wasm_bindgen macro attribute
 #[wasm_bindgen]
-pub fn Gray_Scale(encoded_Image : &str)
+pub fn Gray_Scale(encoded_Image : &str) -> String
 {
     //using the log function from the web_sys cargo package to export a console log function to the JS side of the code and print the resault 
     //adding the into function to convert the string to a JsValue as the log_1 finction takes a JsValue as an argument
@@ -23,5 +26,19 @@ pub fn Gray_Scale(encoded_Image : &str)
 
     img.blur(10.0);
     log_1(&"the image is blurred".into());
+
+    //converting the image to a vector of bytes
+    //the new update if the image cargo pack need the cursor and seek to be mutable
+    let mut buffer = Cursor::new(vec![]);
+    img.write_to(&mut buffer, Png).unwrap();
+    log_1(&"the image is written and created a new image with the demanded effects".into());
+
+    //converting the final image in the buffer object of bytes to a base64 encoded string
+    let final_Image_With_Effects =  encode(&mut buffer);
+
+    //adding the meta data to the string to be able to display the image in the HTML side of the code
+    let final_Image_With_Effects = format!("data:image/png;base64,{}", final_Image_With_Effects);
+
+    return final_Image_With_Effects;
 
 }
