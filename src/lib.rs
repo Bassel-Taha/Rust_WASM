2d6fcbf8c28ac9643e7fc26f6a087175ec79df76
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-04-22 22:10:41 +0200

    -Add image crate dependency to Cargo.toml -using the loadFromMemory function from the image crate in the lib.rs to load the decoded image to a dynamicImage

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 ;
 use base64::decode;
+use image::load_from_memory;
 
 //exporting the Gray_Scale function to the JS side of the code using the wasm_bindgen macro attribute
 #[wasm_bindgen]
@@ -10,6 +11,9 @@ pub fn Gray_Scale(encoded_Image : &str)
     //adding the into function to convert the string to a JsValue as the log_1 finction takes a JsValue as an argument
     log_1(&"the GrayScale function is called".into());
     //decoding the base64 encoded image to a vector of bytes
-    let decoded_Image = decode(encoded_Image).unwrap();
-    log_1(&"the image is decoded and logged to the console".into())
+    let decoded_Image = decode(&encoded_Image).unwrap();
+    log_1(&"the image is decoded and logged to the console".into());
+    //loading the image from the memory
+    let img = load_from_memory(&decoded_Image).unwrap();
+    log_1(&"the image is loaded from the memory".into());
 }
